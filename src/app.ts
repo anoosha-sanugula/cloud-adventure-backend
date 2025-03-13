9ee4f06f38dfe8a8d655bb31170246dc2dfca53c
goldie <goldie@Anoosha-Sanugula.local>
2025-03-13 16:10:00 +0530

    Initialise starting files

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { sequelize } from "./server";
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+export const main = async () => {
+  try {
+    const PORT = process.env.PORT || 4000;
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+    console.log("Connection has been established successfully.");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error: any) {
+    throw new Error(`Unable to connect to the database: ${error.message}`);
+  }
+};
+
+main();
